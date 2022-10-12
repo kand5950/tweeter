e0ca4a1e9d851aab79dc612c29c2c6d5e1331e6f
Anit <anittankandasamy@gmail.com>
2022-10-12 22:58:05 +0000

    minor fixes to tweets.css error in loading page correctly as before. created creatTweetElement using jQuery, created HTML markup using template literals and returns tweet to caller. created renderTweets for taking in an array of tweet objects and appending each into tweet container.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,41 +6,69 @@
 
 $(document).ready(function() {
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1665363013863
-}
-
-const $tweet = createTweetElement(tweetData);
-
-
-        // <article>
-        //   <header>
-        //     <img src="/images/profile-hex.png" alt="avatar"> 
-        //     <span class="tweetName">Your Name</span>
-        //     <span class="tweetTag">@YourName</span>
-
-        //   </header>
-        //   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-        //   <footer>
-        //     10 days ago
-        //     <div class="icons">
-        //     <i class="fa-solid fa-heart"></i>
-        //     <i class="fa-solid fa-retweet"></i>
-        //     <i class="fa-solid fa-flag"></i>
-        //     </div>
-        //   </footer>
-        // </article>
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1665438266794
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1665524666794
+    }
+  ];
 
 
-console.log($tweet);
-$('.tweets').append($tweet);
+  const renderTweets = (tweets) => {
+  // loops through tweets
+    for (let tweet of tweets) {
+    // calls createTweetElement for each tweet
+      let tweetContainer = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('.tweets').append(tweetContainer);
+
+    }
+  };
+
+  const createTweetElement = (data) => {
+    let tweet = `
+  <article>
+    <header>
+      <img class="profile" src="${data.user.avatars}" alt="avatar"> 
+      <span class="tweetName">${data.user.name}</span>
+      <span class="tweetTag">${data.user.handle}</span>
+    </header>
+    <p>${data.content.text}</p>
+    <footer>
+      <span class="data">${data.created_at}</span>
+      <div class="icons">
+      <i class="fa-solid fa-heart"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-flag"></i>
+      </div>
+    </footer>
+  </article>`;
+    return tweet;
+
+  };
+
+
+        
+
+
+  renderTweets(tweetData);
 
 });
